098b83d0020f1c1a13d78cbab2e69e462183f562
unknown <21070252@vnu.edu.vn>
2023-04-12 08:53:55 +0700

    Polishing plate boiler content

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :students
     authenticate :user, lambda { |u| u.admin? } do
       mount Sidekiq::Web => '/sidekiq'
     end
 
 
   devise_for :users
-  root to: 'home#index'
+  root to: 'students#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
